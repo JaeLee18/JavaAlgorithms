ee5651746101171a815922cd6a24c62992a31a95
JaeLee18 <lee2161@purdue.edu>
2016-07-05 00:20:50 +0900

    Generating Random Array

---
+++ src/BubbleSort.java
@@ -0,0 +1,36 @@
+/**
+ * Created by jaejoong on 7/4/16.
+ */
+public class BubbleSort {
+    //Generate a specific amount of array randomly.
+
+    public static int[] randomArray(int size){
+        int arr[] = new int[size];
+        for(int i = 0; i < size; i++){
+            arr[i] = (int)(Math.random()*10+1);
+        }
+        return arr;
+    }
+
+    //Bubble Sorting
+    public void bubblesort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length-1; j++){
+                if(arr[i] < arr[j]){
+
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = BubbleSort.randomArray(4);
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("i: " + i + " arr[i]: " + arr[i]);
+        }
+    }
+
+
+
+
+
+}
